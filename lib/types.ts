f5736ebee4e2cb714b464453d9c92a9fbec4a484
patriciayyzhai <patricia.yy.zhai@gmail.com>
2026-08-02 23:31:59 +0800

    Add journal entry types

---
+++ lib/types.ts
@@ -0,0 +1,13 @@
+export type JournalEntry = {
+  id: string;
+  user_id?: string;
+  class_date: string;
+  feeling: string;
+  clicked: string;
+  correction: string;
+  next_practice: string;
+  memory: string;
+  tags: string[];
+  created_at: string;
+  updated_at: string;
+};
